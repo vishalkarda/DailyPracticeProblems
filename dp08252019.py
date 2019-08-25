1fa1648a16f01768f0e9305d3716efcd1fabe37a
Vishal <vishalkalra01@gmail.com>
2019-08-25 15:00:58 +0530

    Create dp08252019.py

---
+++ dp08252019.py
@@ -0,0 +1,35 @@
+# This problem was recently asked by Uber:
+
+# Given a linked list of integers, remove all consecutive nodes that sum up to 0.
+
+class Node:
+    def __init__(self, val, next=None):
+        self.value = val
+        self.next = next
+
+def removeConsecutiveSumTo0(node):
+    # Fill this in.
+    start = node
+    while node.next:
+        temp = node
+        total = 0
+        while temp:
+            total += temp.value
+            if total == 0:
+                start.next = temp.next
+            temp = temp.next
+        node = node.next
+    return start
+
+
+node = Node(10)
+node.next = Node(5)
+node.next.next = Node(-3)
+node.next.next.next = Node(-3)
+node.next.next.next.next = Node(1)
+node.next.next.next.next.next = Node(4)
+node.next.next.next.next.next.next = Node(-4)
+node = removeConsecutiveSumTo0(node)
+while node:
+    print (node.value, end=' ')
+    node = node.next
