c99fa6aab3eddd317e740c99607953c97dab5a0a
vishal <vishalkalra01@gmail.com>
2019-09-15 22:44:51 +0530

    No of ways

---
+++ 13Sept2019_Find_Ways.py
@@ -0,0 +1,10 @@
+"""
+
+https://stackoverflow.com/questions/53191753/get-react-select-selected-option-when-using-multi-dropdown
+
+https://stackoverflow.com/questions/50090335/how-handle-multiple-select-form-in-reactjs
+
+https://stackoverflow.com/questions/49248560/semantic-ui-react-setting-dropdown-value-to-state
+
+storing multiple values of a react dropdown in a state
+"""
