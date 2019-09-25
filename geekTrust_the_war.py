971e0d86ac4b575736e299943743bfbe674e4ac0
vishal <vishalkalra01@gmail.com>
2019-09-25 22:53:09 +0530

    Geek Trust

--- geekTrust_the_war.py
+++ geekTrust_the_war.py
@@ -1,23 +1,7 @@
 import math
-# class Army:
-#     def __init__(self, H, E, A, SG):
-#         self.horses = H
-#         self.elephant = E
-#         self.tanks = A
-#         self.sling_guns = SG
-#
-#
-# class Lengaburu(Army):
-#     def __init__(self, horse, elephant, tanks, guns):
-#         super().__init__(horse, elephant, tanks, guns)
-#
-#
-# class Falcone(Army):
-#     def __init__(self, horse, elephant, tanks, guns):
-#         super().__init__(horse, elephant, tanks, guns)
 
-# Falicornia attacks with 100 H, 101 E, 20 AT, 5 SG
-data = 'Falicornia attacks with 250 H, 78 E, 20 AT, 15 SG'
+print('Enter the input')
+data = input()
 statement = data.split(' ')
 
 batlions = ['horse', 'elephant', 'tanks', 'guns']
@@ -31,12 +15,11 @@ deployed_fal_army['horse'] = int(statement[3])
 deployed_fal_army['elephant'] = int(statement[5])
 deployed_fal_army['tanks'] = int(statement[7])
 deployed_fal_army['guns'] = int(statement[9])
-print(deployed_fal_army, 'attacking team')
+
 diff['horse'] = math.ceil(deployed_fal_army['horse']/2)
 diff['elephant'] = math.ceil(deployed_fal_army['elephant']/2)
 diff['tanks'] = math.ceil(deployed_fal_army['tanks']/2)
 diff['guns'] = math.ceil(deployed_fal_army['guns']/2)
-print(diff, 'initial')
 
 for value in batlions:
     if diff[value] <= lengaburu_army[value]:
@@ -48,8 +31,6 @@ for value in batlions:
         diff[value] -= lengaburu_army[value]
         lengaburu_army[value] = 0
 
-print(lengaburu_army, 'bachi')
-print(diff, 'fasala')
 for value in range(len(batlions)):
 
     if diff[batlions[value]] != 0:
@@ -62,27 +43,64 @@ for value in range(len(batlions)):
                         lengaburu_army[batlions[value+1]] -= req
                         diff[batlions[value]] = 0
                     else:
-                        print('yes')
-                        deployed_len_army[batlions[value + 1]] = lengaburu_army[batlions[value+1]]
-                        diff[batlions[value+1]] -= lengaburu_army[batlions[value]]
+                        deployed_len_army[batlions[value + 1]] += lengaburu_army[batlions[value+1]]
+                        diff[batlions[value]] -= (lengaburu_army[batlions[value+1]]*2)
                         lengaburu_army[batlions[value]] = 0
+                else:
+                    pass
             elif value == 3:
-                pass
+                req = diff[batlions[value]]*2
+                if lengaburu_army[batlions[value-1]] != 0:
+                    if req <= lengaburu_army[batlions[value-1]]:
+                        deployed_len_army[batlions[value-1]] += req
+                        lengaburu_army[batlions[value-1]] -= req
+                        diff[batlions[value]] = 0
+                    else:
+                        deployed_len_army[batlions[value-1]] += lengaburu_army[batlions[value-1]]
+                        diff[batlions[value]] -= math.ceil(lengaburu_army[batlions[value-1]]*0.5)
+                        lengaburu_army[batlions[value]] = 0
             else:
-                pass
+                req_lower_rank = diff[batlions[value]] * 2
+                req_higher_rank = math.ceil(diff[batlions[value]] / 2)
+                if lengaburu_army[batlions[value-1]] != 0 and req_lower_rank <= lengaburu_army[batlions[value-1]]:
+                    deployed_len_army[batlions[value - 1]] += req_lower_rank
+                    lengaburu_army[batlions[value - 1]] -= req_lower_rank
+                    diff[batlions[value]] = 0
 
+                elif lengaburu_army[batlions[value-1]] != 0 and req_lower_rank > lengaburu_army[batlions[value-1]]:
+                    deployed_len_army[batlions[value - 1]] += lengaburu_army[batlions[value - 1]]
+                    req_lower_rank -= lengaburu_army[batlions[value - 1]]
+                    lengaburu_army[batlions[value - 1]] = 0
+                    diff[batlions[value]] = math.ceil(req_lower_rank/2)
+                    remaining = math.ceil(req_lower_rank/4)
+                    if lengaburu_army[batlions[value + 1]] != 0 and remaining <= lengaburu_army[batlions[value + 1]]:
+                        deployed_len_army[batlions[value+1]] += remaining
+                        lengaburu_army[batlions[value + 1]] -= remaining
+                        diff[batlions[value]] = 0
+                    elif lengaburu_army[batlions[value + 1]] != 0:
+                        deployed_len_army[batlions[value + 1]] += lengaburu_army[batlions[value + 1]]
+                        remaining -= lengaburu_army[batlions[value + 1]]
+                        diff[batlions[value]] = remaining*2
+
+                elif lengaburu_army[batlions[value+1]] != 0 and req_higher_rank <= lengaburu_army[batlions[value+1]]:
+                    deployed_len_army[batlions[value + 1]] += req_higher_rank
+                    lengaburu_army[batlions[value + 1]] -= req_higher_rank
+                    diff[batlions[value]] = 0
+
+                elif lengaburu_army[batlions[value+1]] != 0:
+                    deployed_len_army[batlions[value + 1]] += lengaburu_army[batlions[value + 1]]
+                    diff[batlions[value]] -= (lengaburu_army[batlions[value + 1]] * 2)
+                    lengaburu_army[batlions[value]] = 0
         else:
-            if value == 0:
-                req = math.ceil(diff[batlions[value]] / 2)
-                if lengaburu_army[batlions[value+1]] != 0 & req <= lengaburu_army[batlions[value+1]]:
-                    deployed_len_army[batlions[value]] += req
+            pass
 
 
-print(deployed_len_army, '+ve')
-print(lengaburu_army, 'remaining army')
-print(diff, 'difference')
-# print(f'Lengaburu deploys {horse_r} H {elephant_r} E {tanks_r}'
-#       f' AT {guns_r} SG & wins')
+total = sum(diff.values())
+result = 'wins'
+if total > 0:
+    result = 'loses'
+print(f"Lengaburu deploys {deployed_len_army['horse']} H {deployed_len_army['elephant']} E {deployed_len_army['tanks']}"
+      f" AT {deployed_len_army['guns']} SG & {result}")
 
 
 
