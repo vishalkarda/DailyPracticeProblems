e8ad945e611fad3d187b003d2344e7b00b8737db
vishal <vishalkalra01@gmail.com>
2019-10-06 23:13:12 +0530

    trapping water

--- September/30Sept2019_trapping_water.py
+++ September/30Sept2019_trapping_water.py
@@ -12,21 +12,42 @@ For example: [0,1,0,2,1,0,1,3,2,1,2,1] should return 6 because 6 units of water 
 
 
 def capacity(arr):
-    left = mid = right = counter = 0
-    for i in range(0, len(arr)):
-        if i == 0:
-            continue
+    n = len(arr)
+    # initialize output
+    result = 0
 
-        if i+1 < len(arr):
-            print(f'left {arr[i - 1]} mid {arr[i]} right {arr[i + 1]}')
-            left = arr[i-1]
-            mid = arr[i]
-            right = arr[i+1]
+    # maximum element on left and right
+    left_max = 0
+    right_max = 0
 
-        if mid < left and mid < right:
-            counter += 1
+    # indices to traverse the array
+    lo = 0
+    hi = n - 1
 
-    return counter
+    while lo <= hi:
+
+        if arr[lo] < arr[hi]:
+
+            if arr[lo] > left_max:
+
+                # update max in left
+                left_max = arr[lo]
+            else:
+
+                # water on curr element = max - curr
+                result += left_max - arr[lo]
+            lo += 1
+
+        else:
+
+            if arr[hi] > right_max:
+                # update right maximum
+                right_max = arr[hi]
+            else:
+                result += right_max - arr[hi]
+            hi -= 1
+
+    return result
 
 
 print(capacity([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]))
