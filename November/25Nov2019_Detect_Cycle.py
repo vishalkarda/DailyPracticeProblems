9ae43def46274af4f1e6eae5cf51ec31e70c2b72
vishal <vishalkalra01@gmail.com>
2019-11-27 19:08:19 +0530

    Detect Loop

--- November/25Nov2019_Detect_Cycle.py
+++ November/25Nov2019_Detect_Cycle.py
@@ -20,8 +20,10 @@ class Solution(object):
         slow_p = head
         fast_p = head
         while slow_p and fast_p and fast_p.next:
+
             slow_p = slow_p.next
             fast_p = fast_p.next.next
+            
             if slow_p == fast_p:
                 return True
 
