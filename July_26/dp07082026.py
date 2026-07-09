357ddf2d9012c05099f939c136aaaba002ae3731
vishalkarda <vishalkalra01@gmail.com>
2026-07-09 07:43:33 +0530

    added another vairant for interleaving strings

--- July_26/dp07082026.py
+++ July_26/dp07082026.py
@@ -63,6 +63,25 @@ class Solution:
 
         return dp[0][0]
     
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        n, m = len(s1), len(s2)
+        if n + m != len(s3):
+            return False
+        
+        dp = [[False] * (m + 1) for _ in range(n + 1)]
+        dp[0][0] = True
+
+        for i in range(n + 1):
+            for j in range(m + 1):
+                if i > 0 and dp[i-1][j] and s1[i-1] == s3[i-1+j]:
+                    dp[i][j] = True
+                if j > 0 and dp[i][j-1] and s2[j-1] == s3[i+j-1]:
+                    dp[i][j] = True
+
+        return dp[n][m]
+        
+    
 if __name__ == "__main__":
     s1 = "aabcc"
     s2 = "dbbca"
